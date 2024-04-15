7ba06fad2fd3ed27d41d5aa4b5a64f53f6c648dc
olusola joshua <joolaoluwa2002@gmail.com>
2024-04-15 21:22:38 +0100

    New changes, fixed swagger issue

--- BoardGameApi/Program.cs
+++ BoardGameApi/Program.cs
@@ -1,7 +1,26 @@
+// using BoardGameApi.Extensions;
+using Microsoft.AspNetCore.Cors;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(cfg =>
+    {
+        cfg.AllowAnyMethod();
+        cfg.WithOrigins(builder.Configuration["AllowedOrigins"]);
+        cfg.AllowAnyHeader();
+    });
+
+    options.AddPolicy("AnyOrigin", cfg =>
+    {
+        cfg.AllowAnyOrigin();
+        cfg.AllowAnyMethod();
+        cfg.AllowAnyHeader();
+    });
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -9,18 +28,11 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if(app.Environment.IsDevelopment())
+if(app.Configuration.GetValue<bool>("UseSwagger"))
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-if(app.Environment.IsStaging())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 if(app.Configuration.GetValue<bool>("UseDeveloperExceptionPage"))
     app.UseDeveloperExceptionPage();
 else
@@ -28,9 +40,11 @@ else
 
 
 app.UseHttpsRedirection();
+app.UseCors();
+
+app.MapGet("/error", [EnableCors("AnyOrigin")] () => Results.Problem());
+app.MapGet("/error/test", [EnableCors("AnyOrigin")] () => {throw new Exception("error...");});
 
 app.MapControllers();
-app.MapGet("/error", () => Results.Problem());
-app.MapGet("/error/test", () => {throw new Exception("error...");});
 
 app.Run();
--- BoardGameApi/obj/Debug/net8.0/BoardGameApi.AssemblyInfo.cs
+++ BoardGameApi/obj/Debug/net8.0/BoardGameApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BoardGameApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bdfface078c6d98bee2f93caa1da5ec77625524")]
 [assembly: System.Reflection.AssemblyProductAttribute("BoardGameApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BoardGameApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
