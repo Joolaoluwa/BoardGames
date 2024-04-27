e1cbefe31368397637df5561a6a4c2147edd9945
olusola joshua <joolaoluwa2002@gmail.com>
2024-04-27 15:41:13 +0100

    Error fixed...

--- BoardGameApi/Controller/BoardGameController.cs
+++ BoardGameApi/Controller/BoardGameController.cs
@@ -6,8 +6,7 @@ using BoardGameApi;
 using BoardGameApi.DTOs;
 using Microsoft.AspNetCore.Mvc;
 
-namespace BoardGameApi.Controller
-{
+namespace BoardGameApi.Controller;
     [Route("BoardGameList")]
     [ApiController]
     [ResponseCache(Location = ResponseCacheLocation.Any, Duration = 60)]
@@ -55,44 +54,11 @@ namespace BoardGameApi.Controller
                     LinkDto = new List<LinkDTO>()
                     {
                         new LinkDTO(
-                            Url.Action(null, "BoardGameList", null, Request.Scheme)!,
+                             Url.Action(null, "BoardGameList", null, Request.Scheme)!,
                             "self",
                             "GET"
-                        ),
+                        )
                     }
                 };
-            }
-        }
-        // public IEnumerable<BoardGame> GetGames()
-        // {
-        //     var games = new []
-        //     {
-        //         new BoardGame()
-        //         {
-        //             Id = 1,
-        //             Name = "Axis & Allies",
-        //             Year = 1981,
-        //             MinPlayers = 2,
-        //             MaxPlayers = 5
-        //         },
-        //         new BoardGame()
-        //         {
-        //             Id = 2,
-        //             Name = "Citadels",
-        //             Year = 2000,
-        //             MinPlayers = 2,
-        //             MaxPlayers = 8 
-        //         },
-        //         new BoardGame()
-        //         {
-        //             Id = 3,
-        //             Name = "Terraforming Mars",
-        //             Year = 2016,
-        //             MinPlayers = 1,
-        //             MaxPlayers = 5
-        //         }
-        //     };
-        //     return games;
-        // }
+            } 
     }
-}
