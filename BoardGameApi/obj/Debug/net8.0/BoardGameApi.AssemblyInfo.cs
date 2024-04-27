822c15b6c96e4f37b7ae4bf3a3366e0347056fdc
olusola joshua <joolaoluwa2002@gmail.com>
2024-04-27 14:18:57 +0100

    Added DTO folder

--- BoardGameApi/obj/Debug/net8.0/BoardGameApi.AssemblyInfo.cs
+++ BoardGameApi/obj/Debug/net8.0/BoardGameApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BoardGameApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ba06fad2fd3ed27d41d5aa4b5a64f53f6c648dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1df15629ab0b730eed1daa0fafce9c0b0ca98fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("BoardGameApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BoardGameApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
